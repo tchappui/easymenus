d9971fb0da14587eba735368a7d23032f13a9eb3
Thierry Chappuis <thierry@chappuis.io>
2018-06-12 13:51:59 +0200

    First commit

---
+++ main.py
@@ -0,0 +1,60 @@
+from menus import Menu
+
+data = {
+    'A': ["A1", "A2", "A3", "A4"],
+    'B': ["B1", "B2", "B3", "B4", "B5", "B6", "B7", "B8"],
+    'C': ["C1", "C2", "C3", "C4", "C6"],
+}
+
+class App:
+    def start(self):
+        self.handle_start_menu()
+
+    def handle_start_menu(self, entries={}):
+        menu = Menu('accueil', title='Accueil', prompt='Quelle entrée choisissez-vous? ')
+        menu.add('Choisir un A', self.handle_a_menu, 'a')
+        menu.add('Quitter', self.handle_quit, 'q')
+        menu.manager.ask(entries)
+
+    def handle_a_menu(self, entries={}):
+        menu = Menu('a', title="Menu A", prompt='Quelle entrée choisissez-vous? ')
+        for a in data['A']:
+            menu.add(a, self.handle_b_menu)
+        menu.add('Quitter', self.handle_quit, 'q')
+        menu.add("Revenir à l'accueil", self.handle_start_menu, 'a')
+        menu.manager.ask(entries)
+
+    def handle_b_menu(self, entries={}):
+        menu = Menu('b', title="Menu B", prompt='Quelle entrée choisissez-vous? ')
+        for b in data['B']:
+            menu.add(b, self.handle_c_menu)
+        menu.add('Quitter', self.handle_quit, 'q')
+        menu.add("Revenir à l'accueil", self.handle_start_menu, 'a')
+        menu.add("Revenir en arrière", self.handle_a_menu, 'b')
+        menu.manager.ask(entries)
+
+    def handle_c_menu(self, entries={}):
+        menu = Menu('c', title="Menu C", prompt='Quelle entrée choisissez-vous? ')
+        for c in data['C']:
+            menu.add(c, self.handle_final_print)
+        menu.add('Quitter', self.handle_quit, 'q')
+        menu.add("Revenir à l'accueil", self.handle_start_menu, 'a')
+        menu.add("Revenir en arrière", self.handle_a_menu, 'b')
+        menu.manager.ask(entries)
+
+    def handle_final_print(self, entries={}):
+        print(
+            "Vous avez sélectionné:\n"
+            f"- {entries['accueil'].label} ({entries['accueil'].id})\n"
+            f"-- {entries['a'].label} ({entries['a'].id})\n"
+            f"--- {entries['b'].label} ({entries['b'].id})\n"
+            f"---- {entries['c'].label} ({entries['c'].id})\n"
+        )
+        self.handle_start_menu(entries)
+
+    def handle_quit(self, entries):
+        print("Bye, bye!")
+
+if __name__ == "__main__":
+    app = App()
+    app.start()
---
+++ menus.py
@@ -0,0 +1,78 @@
+class _DefaultFormater:
+    """Class responsible for the formatting of the menus."""
+
+    def format(self, menu):
+        """Formats a menu."""
+        lines = [f'--- {menu.title} ---']
+        lines.extend(f'{k} - {v.label}' for k, v in sorted(menu.numeric_entries.items()))
+        lines.extend(f'{k} - {v.label}' for k, v in sorted(menu.keyword_entries.items()))
+        lines.append(menu.prompt)
+        return '\n'.join(lines)
+
+class MenuEntry:
+    """Class representing a menu entry."""
+
+    def __init__(self, id, label, handler, menu):
+        """Constructor."""
+        self.id = id
+        self.label = label
+        self.handler = handler
+        self.menu = menu
+
+class MenuManager:
+    """Class responsible for the management of a menu."""
+
+    def __init__(self, menu):
+        """Constructor."""
+        self.menu = menu
+
+    def _input(self):
+        """Asks the user for an input and repeat the request until the
+        answer is correct."""
+        while True:
+            answer = input(self.menu.message).lower().strip()
+            if self.menu.is_valid(answer):
+                return self.menu.get(answer)
+
+    def ask(self, entries):
+        """Prompts the user for an input and execute the correct handler method."""
+        menu_entry = self._input()
+        entries[menu_entry.menu.name] = menu_entry
+        menu_entry.handler(entries)
+
+
+class Menu:
+    """Class reprensenting a menu."""
+
+    def __init__(self, name, title=None, prompt='--> ', formatter=None, manager=None):
+        """Constructor."""
+        self.name = name
+        self.counter = 1
+        self.title = title if title else name.title()
+        self.prompt = prompt
+        self.numeric_entries = {}
+        self.keyword_entries = {}
+        self.formater = formatter if formatter else _DefaultFormater()
+        self.manager = manager if manager else MenuManager(self)
+
+    def add(self, label, handler, id=None):
+        """Appends a new entry to the menu. The new entry is numeric by default."""
+        if id is None:
+            id = self.counter
+            self.counter += 1
+            self.numeric_entries[str(id)] = MenuEntry(id, label, handler, self)
+        else:
+            self.keyword_entries[id] = MenuEntry(id, label, handler, self)
+
+    def get(self, answer):
+        """Returns the MenuEntry corresponding to a given answer of the user."""
+        return {**self.numeric_entries, **self.keyword_entries}.get(answer, None)
+
+    def is_valid(self, answer):
+        """Returns True if the user answer is a valid one."""
+        return answer in {**self.numeric_entries, **self.keyword_entries}
+
+    @property
+    def message(self):
+        """Formats the menu."""
+        return self.formater.format(self)
