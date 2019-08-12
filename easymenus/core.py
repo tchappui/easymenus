6f595e343083e41abf552b938846918017ef4073
Thierry Chappuis <tchappui@gmail.com>
2019-08-12 08:56:41 +0200

    Refactored deeply

---
+++ easymenus/core.py
@@ -0,0 +1,79 @@
+from jinja2 import Template
+
+
+_DEFAULT_TEMPLATE = \
+"""{% if menu.title -%}
+--- {{ menu.title }} ---
+{% endif -%}
+{% for key, entry in menu -%}
+{{ key }} - {{ entry.text }}
+{% endfor -%}
+{{ menu.prompt }}"""
+
+
+class MenuEntry:
+    """Class representing a menu entry."""
+
+    def __init__(self, text, handler, key, menu, **data):
+        """Constructor."""
+        self.text = text
+        self.handler = handler
+        self.menu = menu
+        self.key = key
+        self.data = data
+
+class Menu:
+    """Class reprensenting a menu."""
+
+    choices = {}
+
+    def __init__(
+        self, 
+        name, 
+        title, 
+        prompt="--> ",
+        template=Template(_DEFAULT_TEMPLATE),
+    ):
+        """Constructor."""
+        self._counter = 1
+        self._entries = {}
+
+        self.name = name
+        self.title = title
+        self.prompt = prompt
+        self.template = template
+
+        self.choices[name] = None
+
+    def add(self, text, handler, key=None, **data):
+        """Appends a new entry to the menu. The new entry is numeric by default."""
+        if key is None:
+            key = self._counter
+            self._counter += 1
+        key = str(key)
+        self._entries[key] = MenuEntry(text, handler, key, self, **data)
+
+    def is_valid(self, answer):
+        """Returns True if the user answer is a valid one."""
+        return answer in self
+
+    def __call__(self):
+        """Asks the user for an input and repeat the request until the
+        answer is correct."""
+        while True:
+            answer = input(self.template.render(menu=self))
+            if self.is_valid(answer):
+                entry = self.choices[self.name] = self[answer]
+                return entry.handler()
+
+    def __getitem__(self, key):
+        """Returns the MenuEntry corresponding to a given key."""
+        return self._entries.get(str(key))
+
+    def __contains__(self, key):
+        """Returns the MenuEntry corresponding to a given key."""
+        return str(key) in self._entries
+
+    def __iter__(self):
+        return iter(self._entries.items())
+
--- examples/main.py
+++ examples/main.py
@@ -1,4 +1,4 @@
-from easymenus.menus import Menu
+from easymenus.core import Menu
 
 # Fake database results
 data = {
@@ -11,84 +11,57 @@ class Application:
     """Represents the demo application."""
     
     def start(self):
-        """Main entry point of the demo application.
-        
-        The main menu is handled by self.handle_start_menu.
-        """
-        self.handle_start_menu()
-
-    def handle_start_menu(self, entries={}):
-        """Handler method for the main menu.
-        
-        1. Create the menu
-        2. Add menu entries
-        3. Display the menu to the user
-        """
-        menu = Menu('start', title='Start menu', prompt='What do you want to do? ')
+        """Main entry point of the demo application."""
+        menu = Menu('start', title='Start Menu', prompt='What do you want to do ? ')
         menu.add('Choose a A', self.handle_a_menu, 'a')
         menu.add('Quit', self.handle_quit, 'q')
-        menu.manager.ask(entries)
+        menu()
 
-    def handle_a_menu(self, entries={}):
-        """Handler method for the A menu.
-        
-        1. Create the menu
-        2. Add numeric menu entries from the 'pseudo-database'
-        2. Add keyword menu entries to quit the app and to return to the previous menu
-        3. Display the menu to the user
-        """
-        menu = Menu('a', title="Menu A", prompt='What do you want to do? ')
+    def handle_a_menu(self):
+        """Handler method for the A menu."""
+        menu = Menu('a', title="Menu A", prompt='What do you want to do ? ')
         for a in data['A']:
             menu.add(a, self.handle_b_menu)
+        menu.add("Return to start menu", self.start, 'm')
         menu.add('Quit', self.handle_quit, 'q')
-        menu.add("Return to start menu", self.handle_start_menu, 'm')
-        menu.manager.ask(entries)
+        menu()
 
-    def handle_b_menu(self, entries={}):
-        """Handler method for the B menu.
-        
-        1. Create the menu
-        2. Add numeric menu entries from the 'pseudo-database'
-        2. Add keyword menu entries to quit, return to the main menu or return to the previous menu.
-        3. Display the menu to the user
-        """
-        menu = Menu('b', title="Menu B", prompt='What do you want to do? ')
+    def handle_b_menu(self):
+        """Handler method for the B menu."""
+        menu = Menu('b', title="Menu B", prompt='What do you want to do ? ')
         for b in data['B']:
             menu.add(b, self.handle_c_menu)
         menu.add('Quit', self.handle_quit, 'q')
-        menu.add("Return to start menu", self.handle_start_menu, 'a')
+        menu.add("Return to start menu", self.start, 'a')
         menu.add("Return to previous menu", self.handle_a_menu, 'b')
-        menu.manager.ask(entries)
+        menu()
 
-    def handle_c_menu(self, entries={}):
-        """Handler method for the C menu.
-        
-        1. Create the menu
-        2. Add numeric menu entries from the 'pseudo-database'
-        2. Add keyword menu entries to quit, return to the main menu or return to the previous menu.
-        3. Display the menu to the user
-        """
-        menu = Menu('c', title="Menu C", prompt='Quelle entrée choisissez-vous? ')
+    def handle_c_menu(self):
+        """Handler method for the C menu."""
+        menu = Menu('c', title="Menu C", prompt='Quelle entrée choisissez-vous ? ')
         for c in data['C']:
             menu.add(c, self.handle_final_print)
         menu.add('Quit', self.handle_quit, 'q')
-        menu.add("Return to start menu", self.handle_start_menu, 'a')
+        menu.add("Return to start menu", self.start, 'a')
         menu.add("Return to previous menu", self.handle_a_menu, 'b')
-        menu.manager.ask(entries)
+        menu()
 
-    def handle_final_print(self, entries={}):
+    def handle_final_print(self):
         """Handler method for the final task, then returns to the main menu."""
         print(
             "Vous avez sélectionné:\n"
-            f"- {entries['start'].label} ({entries['start'].id})\n"
-            f"-- {entries['a'].label} ({entries['a'].id})\n"
-            f"--- {entries['b'].label} ({entries['b'].id})\n"
-            f"---- {entries['c'].label} ({entries['c'].id})\n"
+            f"- {Menu.choices['start'].text} ({Menu.choices['start'].key})\n"
+            f"-- {Menu.choices['a'].text} ({Menu.choices['a'].key})\n"
+            f"--- {Menu.choices['b'].text} ({Menu.choices['b'].key})\n"
+            f"---- {Menu.choices['c'].text} ({Menu.choices['c'].key})\n"
         )
-        # Retour au point de départ
-        self.handle_start_menu(entries)
+        menu = Menu('last', title='', prompt='What do you what to do ? ')
+        menu.add('Return to the start menu', self.start, 'h')
+        menu.add('Quit', self.handle_quit, 'q')
+        menu()
+        
 
-    def handle_quit(self, entries):
+    def handle_quit(self):
         """Handler method for the quit choice."""
         print("Bye, bye!")
         
